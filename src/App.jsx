53166a80e811a07e257d4059c9c8afbedea80cf2
Brayan <bittolo.8bit@gmail.com>
2026-02-09 22:53:58 +0100

    Basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
 import reactLogo from "./assets/img/react.svg";
 import viteLogo from "/vite.svg";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
